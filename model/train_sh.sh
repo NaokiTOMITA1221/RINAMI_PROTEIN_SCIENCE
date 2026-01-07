d659e7d615a413e12e8ed88b451b2ee49e087960
NaokiTOMITA1221 <tomita.naokinyo@gmail.com>
2026-01-07 21:02:39 +0900

    Rewrite train_sh.sh

--- model/train_sh.sh
+++ model/train_sh.sh
@@ -1,6 +1,6 @@
 mkdir -p ../pth
 
-#python3 RINAMI_foldability_prediction_train_and_test.py ../pth/foldability_predictor_0.pth
+python3 RINAMI_foldability_prediction_train_and_test.py ../pth/foldability_predictor_0.pth
 python3 RINAMI_regression_train_and_test.py ../pth/dG_predictor_0.pth ../pth/foldability_predictor_0.pth
 
 for i in {1..10}
